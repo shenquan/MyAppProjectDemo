d69804cfa1e016a75faa6c85c861975585a1bc3b
韩申权 <sqhan@Ctrip.com>
2017-09-07 14:41:24 +0800

    kotlin

---
+++ kotlinmodule/src/main/java/com/example/kotlinmodule/kotlin/Practice.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinmodule.kotlin
+
+/**
+ * Created by sqhan on 2017/9/7.
+ */
+class Practice {
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun main(args: Array<String>) {
+        print("sum of 3 and 5 is ")
+        println(sum(3, 5))
+    }
+
+}
