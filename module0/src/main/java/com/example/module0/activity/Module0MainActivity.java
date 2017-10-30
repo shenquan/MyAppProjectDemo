ac6946c63db19614d688d4b48b29906c44f92d6e
韩申权 <sqhan@Ctrip.com>
2017-10-30 12:53:58 +0800

    打开联系人，并返回联系人的姓名与电话号码

--- module0/src/main/java/com/example/module0/activity/Module0MainActivity.java
+++ module0/src/main/java/com/example/module0/activity/Module0MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.module0.activity;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.ComponentName;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.TextView;
 
@@ -20,10 +29,18 @@ import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Module0MainActivity extends BaseActivity {
     private Context mContext;
     private TextView mTextMessage;
+    private TextView tv2;
+    private TextView tv3;
+    private TextView tv4;
+    private TextView tv5;
+    private TextView tv6;
 
     /*private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -102,7 +119,7 @@ public class Module0MainActivity extends BaseActivity {
 //        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
 //        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        TextView tv2 = (TextView) findViewById(R.id.tv2);
+        tv2 = (TextView) findViewById(R.id.tv2);
         tv2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -110,7 +127,7 @@ public class Module0MainActivity extends BaseActivity {
                 Bus.callData(mContext, "module1/goMain");
             }
         });
-        TextView tv3 = (TextView) findViewById(R.id.tv3);
+        tv3 = (TextView) findViewById(R.id.tv3);
 //        tv3.setVisibility(View.GONE);
         tv3.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -147,7 +164,7 @@ public class Module0MainActivity extends BaseActivity {
         } catch (JSONException e) {
         }*/
 
-        TextView tv4 = (TextView) findViewById(R.id.tv4);
+        tv4 = (TextView) findViewById(R.id.tv4);
         tv4.setVisibility(View.GONE);
         tv4.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -164,7 +181,7 @@ public class Module0MainActivity extends BaseActivity {
             }
         });
 
-        TextView tv5 = (TextView) findViewById(R.id.tv5);
+        tv5 = (TextView) findViewById(R.id.tv5);
         tv5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -172,8 +189,38 @@ public class Module0MainActivity extends BaseActivity {
             }
         });
 
+        tv6 = (TextView) findViewById(R.id.tv6);
+        tv6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestPermission();//请求相关权限
+                startActivityForResult(new Intent(Intent.ACTION_PICK,
+                        ContactsContract.Contacts.CONTENT_URI), 10);
+            }
+        });
+
 
     }
+
+    //相关权限申请
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            final List<String> permissionsList = new ArrayList<>();
+            addPermission(permissionsList, Manifest.permission.READ_CONTACTS);
+            if (permissionsList.size() > 0) {
+                requestPermissions(permissionsList.toArray(new String[permissionsList.size()]), 100);
+            }
+        }
+    }
+
+    private boolean addPermission(List<String> permissionsList, String permission) {
+        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            permissionsList.add(permission);
+            return false;
+        }
+        return true;
+    }
+
 
     private View vv;
 
@@ -202,4 +249,42 @@ public class Module0MainActivity extends BaseActivity {
         super.onDestroy();
         EventBus.getDefault().unregister(this);
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 10) {
+            try {
+                // ContentProvider展示数据类似一个单个数据库表
+                // ContentResolver实例带的方法可实现找到指定的ContentProvider并获取到ContentProvider的数据
+                ContentResolver reContentResolverol = getContentResolver();
+                // URI,每个ContentProvider定义一个唯一的公开的URI,用于指定到它的数据集
+                Uri contactData = data.getData();
+                // 查询就是输入URI等参数,其中URI是必须的,其他是可选的,如果系统能找到URI对应的ContentProvider将返回一个Cursor对象.
+                Cursor cursor = mContext.getContentResolver().query(contactData, null, null, null, null);
+                cursor.moveToFirst();
+                // 获得DATA表中的名字
+                String username = cursor.getString(cursor
+                        .getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                // 条件为联系人ID
+                String contactId = cursor.getString(cursor
+                        .getColumnIndex(ContactsContract.Contacts._ID));
+                // 获得DATA表中的电话号码，条件为联系人ID,因为手机号码可能会有多个
+                Cursor phone = reContentResolverol.query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = "
+                                + contactId, null, null);
+                String usernumber = "";
+                while (phone.moveToNext()) {
+                    usernumber = phone
+                            .getString(phone
+                                    .getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    tv6.setText(usernumber + " (" + username + ")");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
 }
