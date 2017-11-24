2d8d5bcbb266574a4a9d5ec6fa838440c8bde600
韩申权 <sqhan@Ctrip.com>
2017-11-24 17:24:02 +0800

    kotlin更改版本号

--- kotlinmodule/src/main/java/com/example/kotlinmodule/kotlin/Practice.kt
+++ kotlinmodule/src/main/java/com/example/kotlinmodule/kotlin/Practice.kt
@@ -4,6 +4,19 @@ package com.example.kotlinmodule.kotlin
  * Created by sqhan on 2017/9/7.
  */
 class Practice {
+    var x: Int = 1
+    var y: Double = 1.2
+    var z: String = "3"
+
+    constructor()
+
+    constructor(x: Int, y: Double, z: String) {
+        this.x = x
+        this.y = y
+        this.z = z
+    }
+
+
     fun sum(a: Int, b: Int): Int {
         return a + b
     }
